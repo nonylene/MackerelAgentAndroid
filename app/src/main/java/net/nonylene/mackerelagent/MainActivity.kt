586651e9f4074dcf2fee6db54d14e11d41e08573
nonylene <cookie.speaker@gmail.com>
2016-10-04 20:24:24 +0900

    show load average

---
+++ app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
@@ -0,0 +1,23 @@
+package net.nonylene.mackerelagent
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import net.nonylene.mackerelagent.metric.getLoadAverage
+
+class MainActivity : AppCompatActivity() {
+
+    val textView by lazy {
+        findViewById(R.id.text_view) as TextView
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        textView.text = "loadavg: ${getLoadAverage()}"
+    }
+}
