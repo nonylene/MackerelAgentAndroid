d960d383ecb45d2e679ea99093426034633c1039
nonylene <cookie.speaker@gmail.com>
2016-10-07 00:38:57 +0900

    show disk metrics and dispose onstop

--- app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
+++ app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.BiFunction
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Function3
 import io.reactivex.schedulers.Schedulers
 import io.realm.Realm
 import net.nonylene.mackerelagent.metric.*
 
 class MainActivity : AppCompatActivity() {
+
+    var disposable : Disposable? = null
 
     val textView by lazy {
         findViewById(R.id.text_view) as TextView
@@ -25,9 +28,9 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         val memText = "memory: ${getMemoryInfo()}"
         val loadavgText = "loadavg: ${getLoadAverage5min()}"
-        Observable.combineLatest(getInterfaceDeltaObservable(), getCPUPercentageObservable(),
-                BiFunction { interfaceDeltas: List<InterfaceDelta>, cpuPercentage: CPUPercentage ->
-                    interfaceDeltas to cpuPercentage
+        disposable = Observable.combineLatest(getInterfaceDeltaObservable(), getDiskDeltaObservable(), getCPUPercentageObservable(),
+                Function3 { interfaceDeltas: List<InterfaceDelta>, diskDeltas: List<DiskDelta>, cpuPercentage: CPUPercentage ->
+                    interfaceDeltas to diskDeltas to cpuPercentage
                 })
                 .retryWhen { observable ->
                     // retry once
@@ -39,7 +42,12 @@ class MainActivity : AppCompatActivity() {
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
-                    textView.text = "$loadavgText\n$memText\ninterface: ${it.first}\ncpu: ${it.second}"
+                    textView.text = "$loadavgText\n$memText\ninterface: ${it.first.first}\ndisk: ${it.first.second}\ncpu: ${it.second}"
                 }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        disposable?.dispose()
     }
 }
