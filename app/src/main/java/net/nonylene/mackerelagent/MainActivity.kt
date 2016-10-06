91aeb22f8581b6168a560b3743ea6900dea2d2bf
nonylene <cookie.speaker@gmail.com>
2016-10-06 20:59:18 +0900

    show metrics including interface

--- app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
+++ app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
@@ -3,9 +3,12 @@ package net.nonylene.mackerelagent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
-import net.nonylene.mackerelagent.metric.getCPUPercentageObservable
-import net.nonylene.mackerelagent.metric.getLoadAverage5min
-import net.nonylene.mackerelagent.metric.getMemoryInfo
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Schedulers
+import io.realm.Realm
+import net.nonylene.mackerelagent.metric.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,8 +25,25 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         val memText = "memory: ${getMemoryInfo()}"
         val loadavgText = "loadavg: ${getLoadAverage5min()}"
-        getCPUPercentageObservable().subscribe {
-            textView.text = "$loadavgText\n$memText\ncpu: $it"
-        }
+        Observable.combineLatest(getInterfaceDeltaObservable(), getCPUPercentageObservable(),
+                BiFunction { interfaceDeltas: List<InterfaceDelta>, cpuPercentage: CPUPercentage ->
+                    interfaceDeltas to cpuPercentage
+                })
+                .retryWhen { observable ->
+                    // remove realm cache
+                    observable.doOnNext {
+                        Realm.getDefaultInstance().executeTransaction(Realm::deleteAll)
+                    }
+                    observable.take(1)
+                }
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    textView.text = "$loadavgText\n$memText\ninterface: ${it.first}\ncpu: ${it.second}"
+                }, { error ->
+                    error.printStackTrace()
+                    // remove realm cache
+//                    Realm.getDefaultInstance().executeTransactionAsync(Realm::deleteAll)
+                })
     }
 }
