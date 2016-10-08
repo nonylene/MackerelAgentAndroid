4c676a6f99bdabe50dd89d7e2d48c6729cb6e723
nonylene <cookie.speaker@gmail.com>
2016-10-09 04:14:56 +0900

    show filesystem metrics / throw error after retry

--- app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
+++ app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
@@ -6,14 +6,16 @@ import android.widget.TextView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.functions.Function3
+import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Function
+import io.reactivex.functions.Function4
 import io.reactivex.schedulers.Schedulers
 import io.realm.Realm
 import net.nonylene.mackerelagent.metric.*
 
 class MainActivity : AppCompatActivity() {
 
-    var disposable : Disposable? = null
+    var disposable: Disposable? = null
 
     val textView by lazy {
         findViewById(R.id.text_view) as TextView
@@ -28,21 +30,32 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         val memText = "memory: ${getMemoryInfo()}"
         val loadavgText = "loadavg: ${getLoadAverage5min()}"
-        disposable = Observable.combineLatest(getInterfaceDeltaObservable(), getDiskDeltaObservable(), getCPUPercentageObservable(),
-                Function3 { interfaceDeltas: List<InterfaceDelta>, diskDeltas: List<DiskDelta>, cpuPercentage: CPUPercentage ->
-                    interfaceDeltas to diskDeltas to cpuPercentage
+        disposable = Observable.combineLatest(getInterfaceDeltaObservable(), getDiskDeltaObservable(),
+                getCPUPercentageObservable(), getFileSystemStatsObservable(),
+                Function4 { interfaceDeltas: List<InterfaceDelta>, diskDeltas: List<DiskDelta>,
+                            cpuPercentage: CPUPercentage, fileSystemStats: List<FileSystemStat> ->
+                    interfaceDeltas to diskDeltas to cpuPercentage to fileSystemStats
                 })
                 .retryWhen { observable ->
                     // retry once
-                    observable.doOnNext {
+                    Observable.zip(observable, Observable.range(1, 2), BiFunction { error: Throwable, count: Int ->
+                        error to count
+                    }).flatMap(Function<Pair<Throwable, Int>, Observable<Int>> {
+                        if (it.second > 1) {
+                            Observable.error(it.first)
+                        } else {
+                            Observable.just(it.second)
+                        }
+                    }).doOnNext {
                         // remove realm cache
                         Realm.getDefaultInstance().executeTransaction(Realm::deleteAll)
-                    }.take(1)
+                    }
                 }
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
-                    textView.text = "$loadavgText\n$memText\ninterface: ${it.first.first}\ndisk: ${it.first.second}\ncpu: ${it.second}"
+                    textView.text = "$loadavgText\n$memText\ninterface: ${it.first.first.first}\n" +
+                            "disk: ${it.first.first.second}\ncpu: ${it.first.second}\nfile: ${it.second}"
                 }
     }
 
