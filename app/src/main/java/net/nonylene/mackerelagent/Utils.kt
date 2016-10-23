ca463fccf93cac51e37e8782ee6a74467a9bb443
nonylene <cookie.speaker@gmail.com>
2016-10-23 18:41:18 +0900

    create cpu cpec

---
+++ app/src/main/java/net/nonylene/mackerelagent/Utils.kt
@@ -0,0 +1,21 @@
+package net.nonylene.mackerelagent
+
+import java.util.*
+
+
+inline fun <T> Iterable<T>.splitBy(predicate: (T) -> Boolean): List<List<T>> {
+    val result = ArrayList<List<T>>()
+
+    var entry = ArrayList<T>()
+    for (item in this) {
+       if (predicate(item)) {
+           result.add(entry)
+           entry = ArrayList<T>()
+       } else {
+           entry.add(item)
+       }
+    }
+    result.add(entry)
+
+    return result
+}
