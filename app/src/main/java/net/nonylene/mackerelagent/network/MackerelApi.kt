2284a5613cdcb0b35ff909bc5bf87e00361087ff
nonylene <cookie.speaker@gmail.com>
2016-10-26 01:59:09 +0900

    see api key from preference

--- app/src/main/java/net/nonylene/mackerelagent/network/MackerelApi.kt
+++ app/src/main/java/net/nonylene/mackerelagent/network/MackerelApi.kt
@@ -1,36 +1,49 @@
 package net.nonylene.mackerelagent.network
 
+import android.content.Context
+import android.preference.PreferenceManager
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import net.nonylene.mackerelagent.utils.getApiKey
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object MackerelApi {
 
-    private val client: OkHttpClient
-    private val retrofit: Retrofit
-    val service: MackerelService
+    private var service: MackerelService? = null
 
-    init {
-        // todo: mackerel API Key
-        client = OkHttpClient.Builder()
-                .addInterceptor { chain ->
-                    val request = if (chain.request().url().host() == "mackerel.io") {
-                        chain.request().newBuilder()
-                                .addHeader("X-Api-Key", "value")
-                                .build()
-                    } else {
-                        chain.request()
+    fun getService(context: Context): MackerelService {
+        if (service == null) {
+            val appContext = context.applicationContext
+            // called on every request
+            val apiKeyGetter = {
+                PreferenceManager.getDefaultSharedPreferences(appContext).getApiKey(appContext)
+            }
+
+            val client = OkHttpClient.Builder()
+                    .addInterceptor { chain ->
+                        val request = if (chain.request().url().host() == "mackerel.io") {
+                            apiKeyGetter()?.let {
+                                chain.request().newBuilder()
+                                        .addHeader("X-Api-Key", it)
+                                        .build()
+                            } ?: chain.request()
+                        } else {
+                            chain.request()
+                        }
+                        chain.proceed(request)
                     }
-                    chain.proceed(request)
-                }
-                .build()
+                    .build()
 
-        retrofit = Retrofit.Builder()
-                .baseUrl("https://mackerel.io/")
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        service = retrofit.create(MackerelService::class.java)
+            val retrofit = Retrofit.Builder()
+                    .client(client)
+                    .baseUrl("https://mackerel.io/")
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+
+            service = retrofit.create(MackerelService::class.java)
+        }
+        return service!!
     }
 }
---
+++ app/src/main/java/net/nonylene/mackerelagent/utils/Preferences.kt
@@ -0,0 +1,14 @@
+package net.nonylene.mackerelagent.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import net.nonylene.mackerelagent.R
+
+
+fun SharedPreferences.getApiKey(context: Context): String? {
+    return getString(context.getString(R.string.preference_api_key_key), null)
+}
+
+fun SharedPreferences.Editor.putApiKey(key: String?, context: Context): SharedPreferences.Editor {
+    return putString(context.getString(R.string.preference_api_key_key), key)
+}
