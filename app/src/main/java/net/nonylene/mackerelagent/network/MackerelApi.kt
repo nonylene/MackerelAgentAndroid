44318cd2b5ea0cfc5aa13ec88e97db6350142760
nonylene <cookie.speaker@gmail.com>
2016-10-28 21:09:15 +0900

    show okhttp log

--- app/src/main/java/net/nonylene/mackerelagent/network/MackerelApi.kt
+++ app/src/main/java/net/nonylene/mackerelagent/network/MackerelApi.kt
@@ -8,6 +8,7 @@ import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import net.nonylene.mackerelagent.utils.getApiKey
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -36,6 +37,9 @@ object MackerelApi {
                         }
                         chain.proceed(request)
                     }
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    })
                     .build()
 
             val retrofit = Retrofit.Builder()
