865ba4ccb33019dd7c13595e125857cf00157ea8
nonylene <cookie.speaker@gmail.com>
2016-11-02 23:50:52 +0900

    reset realm before after rebbot or update package

--- app/src/main/java/net/nonylene/mackerelagent/receiver/RebootUpdateReceiver.kt
+++ app/src/main/java/net/nonylene/mackerelagent/receiver/RebootUpdateReceiver.kt
@@ -3,12 +3,17 @@ package net.nonylene.mackerelagent.receiver
 import android.content.Context
 import android.content.Intent
 import android.support.v4.content.WakefulBroadcastReceiver
-import net.nonylene.mackerelagent.service.GatherMetricsService
+import io.realm.Realm
+import net.nonylene.mackerelagent.utils.createGatherMetricsServiceIntent
 
 class RebootUpdateReceiver : WakefulBroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        startWakefulService(context, Intent(context, GatherMetricsService::class.java))
+        // reset realm database (not async !!)
+        Realm.getDefaultInstance().use {
+            it.executeTransaction(Realm::deleteAll)
+        }
+        startWakefulService(context, createGatherMetricsServiceIntent(context))
     }
 
 }
