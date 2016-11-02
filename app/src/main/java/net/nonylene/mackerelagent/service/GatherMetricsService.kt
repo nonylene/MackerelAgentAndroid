c28a28d7e057c97db3e65c48ec88ea716e747fae
nonylene <cookie.speaker@gmail.com>
2016-11-03 00:39:15 +0900

    update host spec on create service

--- app/src/main/java/net/nonylene/mackerelagent/service/GatherMetricsService.kt
+++ app/src/main/java/net/nonylene/mackerelagent/service/GatherMetricsService.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.preference.PreferenceManager
 import android.support.v4.content.WakefulBroadcastReceiver
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
@@ -14,7 +15,9 @@ import net.nonylene.mackerelagent.MainActivity
 import net.nonylene.mackerelagent.R
 import net.nonylene.mackerelagent.network.MackerelApi
 import net.nonylene.mackerelagent.network.model.createMetrics
+import net.nonylene.mackerelagent.utils.createHostSpecRequest
 import net.nonylene.mackerelagent.utils.createMetricsCombineLatestObservable
+import net.nonylene.mackerelagent.utils.getHostId
 import net.nonylene.mackerelagent.utils.retryWith
 import java.util.concurrent.TimeUnit
 
@@ -37,6 +40,14 @@ class GatherMetricsService : Service() {
                 .setContentIntent(pendingIntent)
                 .build()
         startForeground(NOTIFY_ID, notification)
+
+        // update host spec onCreate
+        MackerelApi.getService(this)
+                .updateHostSpec(PreferenceManager.getDefaultSharedPreferences(this).getHostId(this)!!, createHostSpecRequest())
+                .subscribeOn(Schedulers.io())
+                .subscribe({
+                    // do nothing
+                }, Throwable::printStackTrace)
 
         disposable = Observable.interval(0, 1, TimeUnit.MINUTES)
                 .flatMap {
--- app/src/main/java/net/nonylene/mackerelagent/utils/HostUtils.kt
+++ app/src/main/java/net/nonylene/mackerelagent/utils/HostUtils.kt
@@ -1,8 +1,12 @@
 package net.nonylene.mackerelagent.utils
 
+import android.os.Build
 import io.reactivex.Observable
 import io.reactivex.functions.Function3
+import net.nonylene.mackerelagent.BuildConfig
 import net.nonylene.mackerelagent.host.metric.*
+import net.nonylene.mackerelagent.host.spec.*
+import net.nonylene.mackerelagent.network.model.HostSpecRequest
 
 // create observable with take 1 (do not repeat / do not overwrite)
 fun createMetricsCombineLatestObservable(take: Long = 1): Observable<List<MetricsContainer>> {
@@ -15,4 +19,18 @@ fun createMetricsCombineLatestObservable(take: Long = 1): Observable<List<Metric
                 (interfaceDeltas + diskDeltas + cpuPercentage + getFileSystemMetricsList() + getMemoryMetrics() + getLoadAverageMetrics()) as List<MetricsContainer>
             })
             .take(take)
+}
+
+fun createHostSpecRequest(): HostSpecRequest {
+    return HostSpecRequest(
+            "${Build.MANUFACTURER} ${Build.MODEL}",
+            HostSpecRequest.Meta(
+                    BuildConfig.VERSION_NAME,
+                    getBlockDevicesSpec(),
+                    getCPUSpec(),
+                    getFileSystemsSpec(),
+                    getKernelSpec(),
+                    getMemorySpec()
+            )
+    )
 }
