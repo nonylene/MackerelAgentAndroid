5964f2d66938e3ea80183364b18d2812aae7a88a
nonylene <cookie.speaker@gmail.com>
2016-11-02 19:38:49 +0900

    create observable in onCreate
    onStartCommand may be called multiple times

--- app/src/main/java/net/nonylene/mackerelagent/service/GatherMetricsService.kt
+++ app/src/main/java/net/nonylene/mackerelagent/service/GatherMetricsService.kt
@@ -6,40 +6,61 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.support.v4.content.WakefulBroadcastReceiver
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import io.realm.Realm
 import net.nonylene.mackerelagent.MainActivity
 import net.nonylene.mackerelagent.R
+import net.nonylene.mackerelagent.network.MackerelApi
+import net.nonylene.mackerelagent.network.model.createMetrics
+import net.nonylene.mackerelagent.utils.createMetricsCombineLatestObservable
+import net.nonylene.mackerelagent.utils.retryWith
+import java.util.concurrent.TimeUnit
 
 class GatherMetricsService : Service() {
+
+    var disposable: Disposable? = null
 
     companion object {
         const val NOTIFY_ID = 1
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // auto restart
-        try {
-            val notificationIntent = Intent(applicationContext, MainActivity::class.java)
-            val pendingIntent = PendingIntent.getActivity(applicationContext, 0, notificationIntent, 0)
-            val notification = Notification.Builder(this)
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentTitle("konnnitiha")
-                    .setContentText("aaaa")
-                    .setContentIntent(pendingIntent)
-                    .build()
-            startForeground(NOTIFY_ID, notification)
-        } finally {
-            intent?.let(WakefulBroadcastReceiver::completeWakefulIntent)
-        }
-        return START_STICKY
-    }
-
     override fun onCreate() {
         super.onCreate()
-        println("konnnitiha service!")
+        val notificationIntent = Intent(applicationContext, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, notificationIntent, 0)
+        val notification = Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("konnnitiha")
+                .setContentText("aaaa")
+                .setContentIntent(pendingIntent)
+                .build()
+        startForeground(NOTIFY_ID, notification)
+
+        disposable = Observable.interval(0, 1, TimeUnit.MINUTES)
+                .flatMap {
+                    createMetricsCombineLatestObservable() }
+                .retryWith(1) {
+                    // remove realm cache
+                    Realm.getDefaultInstance().use {
+                        it.executeTransaction(Realm::deleteAll)
+                    }
+                }
+                .flatMap { MackerelApi.getService(this).postMetrics(createMetrics(it, this)) }
+                .subscribeOn(Schedulers.io())
+                // do nothing (error -> crash)
+                .subscribe()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.let(WakefulBroadcastReceiver::completeWakefulIntent)
+        return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        disposable?.dispose()
     }
 
     override fun onBind(intent: Intent?): IBinder? {
