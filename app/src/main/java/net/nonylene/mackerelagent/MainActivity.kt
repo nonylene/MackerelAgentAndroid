c44514bdc527c2bf8757d1c15f9d6d5a0763be0e
nonylene <cookie.speaker@gmail.com>
2016-12-19 07:28:30 +0900

    update kotlin

--- app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
+++ app/src/main/java/net/nonylene/mackerelagent/MainActivity.kt
@@ -8,31 +8,17 @@ import android.support.annotation.StringRes
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
 import net.nonylene.mackerelagent.databinding.ActivityMainBinding
 import net.nonylene.mackerelagent.utils.isGatherMetricsServiceRunning
-import net.nonylene.mackerelagent.viewmodel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    val textView by lazy {
-        findViewById(R.id.text_view) as TextView
-    }
-
     lateinit var binding: ActivityMainBinding
-    // todo: after bug fixed (kotlin 1.0.5?), remove model from member variables
-    val model: MainActivityViewModel = MainActivityViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        @Suppress("MISSING_DEPENDENCY_CLASS")
-        binding.model = model
-        model.status.set(if (isGatherMetricsServiceRunning(this)) Status.RUNNING else Status.NOT_RUNNING)
-    }
-
-    override fun onStart() {
-        super.onStart()
+        binding.model.status.set(if (isGatherMetricsServiceRunning(this)) Status.RUNNING else Status.NOT_RUNNING)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -45,11 +31,6 @@ class MainActivity : AppCompatActivity() {
             R.id.menu_activity_main_preference -> startActivity(Intent(this, MackerelPreferenceActivity::class.java))
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    override fun onStop() {
-        super.onStop()
-//        disposable?.dispose()
     }
 
     enum class Status(@StringRes val text: Int, @ColorRes val color: Int) {
