6a5d810ef9cb5e8e5ddd369003d803fb52256b44
nonylene <cookie.speaker@gmail.com>
2016-12-21 23:43:50 +0900

    add migration code (delete all)

--- app/src/main/java/net/nonylene/mackerelagent/MackerelAgentApplication.kt
+++ app/src/main/java/net/nonylene/mackerelagent/MackerelAgentApplication.kt
@@ -2,11 +2,17 @@ package net.nonylene.mackerelagent
 
 import android.app.Application
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
-class MackerelAgentApplication: Application() {
+class MackerelAgentApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+        Realm.setDefaultConfiguration(RealmConfiguration.Builder()
+                .schemaVersion(2L)
+                .deleteRealmIfMigrationNeeded()
+                .build()
+        )
     }
 }
